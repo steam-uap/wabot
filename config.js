9dfe049560e8a9ec76594425bd91e1b17f4d8880
steam-uap <96503080+steam-uap@users.noreply.github.com>
2022-01-17 21:26:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ global.APIKeys = { // APIKey Here
   'https://leyscoders-api.herokuapp.com': 'dappakntlll',
   'https://dhnjing.xyz': 'e2645065aaced89861b4',
   'https://neoxr-api.herokuapp.com': 'yntkts',
-  'https://velgrynd.herokuapp.com': ' ' // Please register if you want to get apikey 
+  'https://velgrynd.herokuapp.com': 'kohalip' // Please register if you want to get apikey 
 }
 
 // Sticker WM
