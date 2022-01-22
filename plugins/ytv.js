da453494a7702301bf220c4739059313863b0403
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-01-23 02:18:44 +0700

    Update ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -1,43 +1,32 @@
 let limit = 30
+let yts = require('yt-search')
 let fetch = require('node-fetch')
-const { servers, ytv } = require('../lib/y2mate')
+
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
-  let chat = global.db.data.chats[m.chat]
-  let server = (args[1] || servers[0]).toLowerCase()
-  let { dl_link, thumb, title, filesize, filesizeF} = await ytv(args[0], servers.includes(server) ? server : servers[0])
-  let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-  conn.sendFile(m.chat, thumb, 'thumbnail.jpg', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
+	if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+	let chat = db.data.chats[m.chat]
+	let { videos } = await yts(args[0])
+	let dl_link = `https://yt-downloader.akkun3704.repl.co/?url=${args[0]}&filter=&quality=&contenttype=`
+	let res = await (await fetch(dl_link)).buffer()
+	let isLimit = (isPrems || isOwner ? 99 : limit) * 1000000 < res.length
+	conn.sendFile(m.chat, videos[0].image, 'thumbnail.jpg', `
+*Title:* ${videos[0].title}
+*Upload:* ${videos[0].ago}
+*Views:* ${videos[0].views}
 *${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
 `.trim(), m)
-  let _thumb = {}
-  try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } }
-  catch (e) { }
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp4', `
-*Title:* ${title}
-*Filesize:* ${filesizeF}
-`.trim(), m, false, {
-  ..._thumb,
-  asDocument: chat.useDocument
-})
+	let _thumb = {}
+	try { _thumb = { thumbnail: await (await fetch(videos[0].image)).buffer() } }
+	catch (e) { }
+	if (!isLimit) conn.sendFile(m.chat, res, videos[0].title + '.mp4', `
+*Title:* ${videos[0].title}
+*Upload:* ${videos[0].ago}
+*Views:* ${videos[0].views}
+`.trim(), m, false, { ..._thumb, asDocument: chat.useDocument })
 }
-handler.help = ['mp4','v',''].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
+handler.help = ['mp4','v',''].map(v => 'yt' + v)
 handler.tags = ['downloader']
 handler.command = /^yt(v|mp4)?$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-handler.exp = 0
 handler.limit = true
 
 module.exports = handler
-
