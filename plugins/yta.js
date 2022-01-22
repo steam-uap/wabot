bef482cd7cbfe2794a4517cb010ff8eb832a4d0e
Ripp <75048902+Akkun3704@users.noreply.github.com>
2022-01-23 02:55:44 +0700

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -4,7 +4,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, args, isPrems, isOwner }) => {
 	if (!args || !args[0]) throw 'Uhm... urlnya mana?'
 	let chat = db.data.chats[m.chat]
-	let dl_link = `https://yt-downloader.akkun3704.repl.co/?url=${args[0]}&filter=audioonly&quality=&contenttype=audio/mp3`
+	let dl_link = `https://yt-downloader.akkun3704.repl.co/?url=${args[0]}&filter=audioonly&quality=&contenttype=`
 	let json = await (await fetch(`https://yt-downloader.akkun3704.repl.co/yt?url=${args[0]}`)).json()
 	let res = await (await fetch(dl_link)).buffer()
 	let isLimit = (isPrems || isOwner ? 99 : limit) * 1000000 < res.length
@@ -15,7 +15,7 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
 *Likes:* ${json.result.videoDetails.likes}
 *${isLimit ? 'Pakai ': ''}Link:* ${dl_link}
 `.trim(), m)
-	if (!isLimit) conn.sendFile(m.chat, res, json.result.videoDetails.title + '.mp3', '', m, false, { asDocument: chat.useDocument })
+	if (!isLimit) conn.sendMessage(m.chat, res, chat.useDocument ? 'documentMessage' : 'audioMessage', { quoted: m, filename: json.result.videoDetails.title + '.mp3', mimetype: 'audio/mp4' })
 }
 handler.help = ['mp3', 'a'].map(v => 'yt' + v)
 handler.tags = ['downloader']
