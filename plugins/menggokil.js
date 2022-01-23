b92ce42b90d4b4aa42894785a4158a625d6ab760
Eʀɪꜱ ` 甘い <87053694+Rlxfly@users.noreply.github.com>
2022-01-23 18:09:40 +0700

    Antek2 alip

--- plugins/menggokil.js
+++ plugins/menggokil.js
@@ -1,25 +1,13 @@
-
 let handler = async (m, { conn, args, command, usedPrefix }) => {
-  if (!args[0]) return m.reply(`Reply video dengan command /${command}`)
+	if (!args[0]) return m.reply(`Reply video dengan command /${command}`)
 	let q = m.quoted ? m.quoted : m
 	let mime = (q.msg || q).mimetype || ''
-	if (/video/.test(mime))
-	
-	{
-		let vid = m.quoted ? { message: { [m.quoted.mtype]: m.quoted }} : m
-m.reply('Loading')
-		 await conn.downloadAndSaveMediaMessage(vid, './src/sw').then(() => m.reply('Sending...'))
+	if (/video/.test(mime)) {
+		let vid = await q.download()
+		m.reply('Loading')
+		await conn.sendMessage(m.chat, vid, 'videoMessage', { quoted: m, duration: args[0] || 9 })
 	} else throw 'Reply videonya!'
-let dur = args[0] || '9'
-let isi = require('fs').readFileSync ('./src/sw.mp4')
-await conn.sendFile(m.chat, isi, 'pler' + '.mp4', `
- Y
- © ${conn.user.name}
-`.trim(), m, false, {
-  
-ptt: false, duration: dur, thumbnail: global.thumb })
 }
-
 handler.help = ['bugv', 'bugvideo']
 handler.tags = ['tools']
 handler.command = /^bug(v|video)$/i
