db6fe9362f24f562c370fde59d5d5c5f4f60c6a0
LitRHap <73509282+LitRHap@users.noreply.github.com>
2022-01-30 20:50:27 +0700

    update bdsm.js

--- plugins/bdsm.js
+++ plugins/bdsm.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   m.reply('Loading...')
-  let res = `https://api.ichikaa.xyz/api/randomimage?q=bdsm&apikey=kohalip`
+  let res = await fetch(API('Velgrynd', '/api/bdsm', { url: args[0] }, 'apikey'))
   conn.sendFile(m.chat, res, 'bdsm.jpg', 'huu suka ama bdsm', m)
 }
 handler.help = ['bdsm']
