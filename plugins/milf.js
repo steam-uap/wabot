bd8756cfff482c95eb8c89e4b70839894d737c6d
LitRHap <73509282+LitRHap@users.noreply.github.com>
2022-01-30 20:59:43 +0700

    update milf.js

--- plugins/milf.js
+++ plugins/milf.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   m.reply('Loading...')
-  let res = `https://api.ichikaa.xyz/api/randomimage/milf?apikey=79lJ9HEA`
+  let res = await fetch(API('Velgrynd', '/api/milf', { url: args[0] }, 'apikey'))
   conn.sendFile(m.chat, res, 'milf.jpg', 'huu suka ama milf', m)
 }
 handler.help = ['milf']
