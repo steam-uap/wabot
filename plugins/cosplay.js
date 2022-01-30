b93fbaa10d7d809ee8c1490166772a5da1ec543f
LitRHap <73509282+LitRHap@users.noreply.github.com>
2022-01-30 20:51:25 +0700

    update cosplay.js

--- plugins/cosplay.js
+++ plugins/cosplay.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   m.reply('Loading...')
-  let res = `https://api.ichikaa.xyz/api/randomimage/cosplay?apikey=kohalip`
+  let res = await fetch(API('Velgrynd', '/api/cosplay', { url: args[0] }, 'apikey'))
   conn.sendFile(m.chat, res, 'cosplay.jpg', 'huu suka ama cosplayer', m)
 }
 handler.help = ['cosplay']
