538434a8e5e5ea88af2e377aabd89355258c31ab
LitRHap <73509282+LitRHap@users.noreply.github.com>
2022-01-30 20:55:11 +0700

    update husbu.js

--- plugins/husbu.js
+++ plugins/husbu.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   m.reply('Loading...')
-  let res = `https://api.ichikaa.xyz/api/randomimage/husbu?apikey=kohalip`
+  let res = await fetch(API('Velgrynd', '/api/husbu', { url: args[0] }, 'apikey'))
   conn.sendFile(m.chat, res, 'husbu.jpg', 'huu suka ama husbu', m)
 }
 handler.help = ['husbu']
