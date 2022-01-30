b15552720684a9f0b533499ee3645e579393f94e
LitRHap <73509282+LitRHap@users.noreply.github.com>
2022-01-30 20:58:44 +0700

    update maid.js

--- plugins/maid.js
+++ plugins/maid.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   m.reply('Loading...')
-  let res = `https://api.ichikaa.xyz/api/randomimage?q=maid&apikey=kohalip`
+  let res = await fetch(API('Velgrynd', '/api/maid', { url: args[0] }, 'apikey'))
   conn.sendFile(m.chat, res, 'maid.jpg', 'huu suka ama maid', m)
 }
 handler.help = ['maid']
